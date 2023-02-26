4772df8e738c67c63c61b5594149377872ba3b9e
cretara <pierdanielecretara@lviiier.com>
2023-02-26 14:56:06 +0100

    Initial commit

---
+++ src/main/java/dev/cretara/spring3newfeatures/Spring3NewFeaturesApplication.java
@@ -0,0 +1,13 @@
+package dev.cretara.spring3newfeatures;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring3NewFeaturesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring3NewFeaturesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/cretara/spring3newfeatures/Spring3NewFeaturesApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.cretara.spring3newfeatures;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring3NewFeaturesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
