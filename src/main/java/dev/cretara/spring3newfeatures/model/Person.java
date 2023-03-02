08e7846f2da9aae5231971be8fcd1913f4858607
cretara <pierdanielecretara@lviiier.com>
2023-03-02 14:02:46 +0100

    WIP data initialization: fix not-null property references a null or transient value

--- src/main/java/dev/cretara/spring3newfeatures/model/Person.java
+++ src/main/java/dev/cretara/spring3newfeatures/model/Person.java
@@ -1,16 +1,19 @@
 package dev.cretara.spring3newfeatures.model;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 @Entity
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
+@EntityListeners(AuditingEntityListener.class)
 public class Person extends BaseModelEntity {
 
     private String name;
